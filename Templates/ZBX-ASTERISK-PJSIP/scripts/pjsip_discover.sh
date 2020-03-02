6594652dd99356957425f22fcf4652478ab3d6ea
anutator <bestann@yandex.ru>
2020-03-02 12:06:17 +0300

    Update pjsip_discover.sh

--- Templates/ZBX-ASTERISK-PJSIP/scripts/pjsip_discover.sh
+++ Templates/ZBX-ASTERISK-PJSIP/scripts/pjsip_discover.sh
@@ -4,7 +4,7 @@
 # v1.0 -- Sebastien Bourgeois <sb@altho.st>
 # This script is used for the discovery part of the template.
 
-### Path of Asterisk binary
+### Путь к исполяемому бинарнику Asterisk
 ASTERISK=/usr/sbin/asterisk
 
 $ASTERISK -rx "pjsip show endpoints"|grep Contact|egrep -v "MaxContact|RTT\(ms\)"|awk '{print $2}'|cut -d "/" -f1 > /tmp/list
