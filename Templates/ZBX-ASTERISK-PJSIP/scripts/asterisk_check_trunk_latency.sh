11d30d6af523024de2ce2ada74958cb14bc42017
anutator <bestann@yandex.ru>
2020-03-03 09:47:59 +0300

    Update asterisk_check_trunk_latency.sh

--- Templates/ZBX-ASTERISK-PJSIP/scripts/asterisk_check_trunk_latency.sh
+++ Templates/ZBX-ASTERISK-PJSIP/scripts/asterisk_check_trunk_latency.sh
@@ -10,5 +10,8 @@ if [ ! -n "$1" ]; then
     echo "Необходим аргумент."
     exit 1
 fi
-
+# pjsip show endpoints — отобразятся данные и по телефонам, и по транкам pjsip
+# grep Contact — т.к. в данных по несколько строк, избавляемся от лишних. Оставляем только строки, содержащие слово Contact
+# egrep -v "MaxContact|RTT\(ms\)" — расширенный поиск, параметр -v — наоборот исключаем строки, содержащие слова MaxContact или RTT(ms)
+# так мы исключили две верхние строки с заголовками, оставив только список телефонов и транковых групп
 $ASTERISK -rx "pjsip show endpoints"|grep Contact|egrep -v "MaxContact|RTT\(ms\)"|grep "$1"|awk '{print $5}'
