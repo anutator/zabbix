34813bf8b3d59e5c3472d9ab3cf84b45cc52d9dd
anutator <bestann@yandex.ru>
2020-03-04 15:33:45 +0300

    Update pjsip_discover.sh

--- Templates/ZBX-ASTERISK-PJSIP/scripts/pjsip_discover.sh
+++ Templates/ZBX-ASTERISK-PJSIP/scripts/pjsip_discover.sh
@@ -9,5 +9,6 @@ ASTERISK=/usr/sbin/asterisk
 
 # Сохраним в файл /tmp/list список транковых групп.
 # Внимание: здесь предполагается, что транковые группы содержат буквы, а телефоны только номера
+# Для этого я добавила grep [A-Za-z]. Иначе сохранялся список телефонов + транковых групп
 $ASTERISK -rx "pjsip show endpoints"|grep Contact|egrep -v "MaxContact|RTT\(ms\)"|awk '{print $2}'|cut -d "/" -f1 | grep [A-Za-z] > /tmp/list
 /usr/bin/jq -nR 'reduce inputs as $i ([]; .+[$i]) | map ({"{#TRUNKNAME}":.}) | {data: .}' /tmp/list
